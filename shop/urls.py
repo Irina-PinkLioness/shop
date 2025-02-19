067226882f27659acc3e6efa80f41c50b684301c
Irina <irina.baryshnikova.fox@hotmail.com>
2025-02-19 22:59:45 +0500

    create django project shop

---
+++ shop/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path, include
+admin.site.site_title = 'On-line shop'
+admin.site.site_header = 'On-line shop'
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('backend.urls'))
+]
